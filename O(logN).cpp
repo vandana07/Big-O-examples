0e4accfdfa609a445c08deb0361f7a59ea0f789a
Aakanksha Jain <aakanksha.jain8@gmail.com>
2018-08-02 20:22:36 +0530

    Create O(logN).cpp

---
+++ O(logN).cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int count, i, arr[30], num, first, last, middle;
+	cout<<"how many elements:"; 
+        cin>>count;
+
+	for (i=0; i<count; i++)
+	{
+		cout<<"Enter number "<<(i+1)<<": "; 
+                cin>>arr[i];
+	}
+	cout<<"Enter the Number to search:"; 
+        cin>>num;
+	first = 0;
+	last = count-1;
+	middle = (first+last)/2;
+	while (first <= last)
+	{
+	   if(arr[middle] < num)
+	   {
+		first = middle + 1;
+
+	   }
+	   else if(arr[middle] == num)
+	   {
+		cout<<num<<" found at the location "<<middle+1<<"\n"; 
+                break; 
+           } 
+           else { 
+                last = middle - 1; 
+           } 
+           middle = (first + last)/2; 
+        } 
+        if(first > last)
+	{
+	   cout<<num<<" not found ";
+	}
+	return 0;
+}
